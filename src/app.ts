f4a374b29c31f0fae3a3cf56f215d4ae002524eb
sunny <goodluck14115@gmail.com>
2025-10-29 17:47:59 +0530

    Add build cmd

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from 'cors'
 import express, { Request, Response } from 'express'
 import morgan from 'morgan'
+import path from 'path'
 
 import noteRouter from './routes/note.route'
 import userRouter from './routes/user.route'
@@ -15,12 +16,38 @@ app.use(express.json())
 app.use(cors())
 app.use(morgan('tiny'))
 
-app.get('/', (_req: Request, res: Response) => {
-  res.send('Hello World!')
-})
-
 app.use('/api/users', userRouter)
 app.use('/api/notes', noteRouter)
+
+// React App
+app.use(
+  express.static(path.join(__dirname, '../web/dist'), {
+    setHeaders(res, filePath) {
+      res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin')
+      res.setHeader('X-Content-Type-Options', 'nosniff')
+
+      if (filePath.endsWith('index.html')) {
+        res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
+        res.setHeader('Pragma', 'no-cache')
+        res.setHeader('Expires', '0')
+      } else {
+        res.setHeader('Cache-Control', 'public, max-age=31536000, immutable')
+      }
+    },
+  })
+)
+
+app.use((req: Request, res: Response, next) => {
+  if (req.path.startsWith('/api')) {
+    return next()
+  }
+
+  res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin')
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
+  res.setHeader('X-Content-Type-Options', 'nosniff')
+
+  res.sendFile(path.join(__dirname, '../web/dist/index.html'))
+})
 
 app.use(errorHandler)
 app.use(unknownEndpoint)
